a50e5e47ebf8a5669b00b87ea788b6683f6ac914
REscalonaJR <esca0063@student.acitech.org>
2025-03-18 07:40:59 -0400

    z

---
+++ rps.js
@@ -0,0 +1,10 @@
+const paper = document.getElementById("paper")
+const rock = document.getElementById("rock")
+const scissors = document.getElementById("scissors")
+const microwave = document.getElementById("microwave")
+const tinfoil = document.getElementById("tinfoil")
+
+
+
+
+
