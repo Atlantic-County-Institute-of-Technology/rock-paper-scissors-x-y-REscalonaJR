5f1605e4dbe4160fa651453dbf943956486db921
esca0063@student.acitech.org <esca0063@student.acitech.org>
2025-03-20 09:08:10 -0400

    i hate javascript

--- rps.js
+++ rps.js
@@ -3,8 +3,22 @@ const rock = document.getElementById("rock")
 const scissors = document.getElementById("scissors")
 const microwave = document.getElementById("microwave")
 const tinfoil = document.getElementById("tinfoil")
+const buttons = document.querySelectorAll("button")
 
+for (let index = 0; index < buttons.length; index++) {
+    buttons[index].addEventListener("click", () => {
+        buttons.forEach(Element => {
+            Element.style.display = "none"
+        })
+    })
+    
+}
+function pickpaper {
+    
+}
 
-
-
-
+paper.addEventListener("click",pickpaper)
+rock.addEventListener("click",pickrock)
+scissors.addEventListener("click",pickscissors)
+microwave.addEventListener("click",pickmicrowave)
+tinfoil.addEventListener("click",picktinfoil)
