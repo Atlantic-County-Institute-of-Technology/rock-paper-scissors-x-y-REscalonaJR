c1fda68ee655f3213aa2d0d68a30bc45beb8d2fe
esca0063@student.acitech.org <esca0063@student.acitech.org>
2025-03-20 09:08:45 -0400

    f

--- rps.js
+++ rps.js
@@ -4,6 +4,9 @@ const scissors = document.getElementById("scissors")
 const microwave = document.getElementById("microwave")
 const tinfoil = document.getElementById("tinfoil")
 const buttons = document.querySelectorAll("button")
+let player = document.getElementById("player")
+const choices = ('rock','paper','scissors','microwave','tinfoil')
+let playerpick = ''
 
 for (let index = 0; index < buttons.length; index++) {
     buttons[index].addEventListener("click", () => {
@@ -11,10 +14,31 @@ for (let index = 0; index < buttons.length; index++) {
             Element.style.display = "none"
         })
     })
+}
+
+
+
+
+
+function pickpaper() {
+    player.innerHTML = '<img src="assets/paper.png" alt="" class="display">';
+     playerpick = "paper"
+
+     /*roberto is roberto*/
     
 }
-function pickpaper {
+function pickrock() {
+    player.innerHTML = '<img src="assets/rock.png" alt="" class="display">'
     
+}
+function pickscissors() {
+    player.innerHTML = '<img src="assets/scissors.png" alt="" class="display">'
+}
+function pickmicrowave() {
+    player.innerHTML = '<img src="assets/microwave.png" alt="" class="display_micro">'
+}
+function picktinfoil() {
+    player.innerHTML = '<img src="assets/tin_foil.png" alt="" class="display">'
 }
 
 paper.addEventListener("click",pickpaper)
