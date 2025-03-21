dbf408e5ea2b440924185f1aad821b0a4d8e62c9
esca0063@student.acitech.org <esca0063@student.acitech.org>
2025-03-21 09:15:24 -0400

    yayayayayayayayyayayayayayayayyayayayayayayyayayay

--- rps.js
+++ rps.js
@@ -5,8 +5,11 @@ const microwave = document.getElementById("microwave")
 const tinfoil = document.getElementById("tinfoil")
 const buttons = document.querySelectorAll("button")
 let player = document.getElementById("player")
-const choices = ('rock','paper','scissors','microwave','tinfoil')
-let playerpick = ''
+let playerpick = 0
+let player_score = document.getElementById("player_score")
+let playerwins = 0
+let computer = document.getElementById("computer")
+let result = document.getElementById("result")
 
 for (let index = 0; index < buttons.length; index++) {
     buttons[index].addEventListener("click", () => {
@@ -17,15 +20,33 @@ for (let index = 0; index < buttons.length; index++) {
 }
 
 
-
+function computerchoice() {
+    return Math.floor(Math.random() * (5-1)) + 1
+}
 
 
 function pickpaper() {
     player.innerHTML = '<img src="assets/paper.png" alt="" class="display">';
-     playerpick = "paper"
-
+     playerpick = 1
+    cpuchoice = computerchoice()
      /*roberto is roberto*/
     
+     if (cpuchoice == 1 ) {
+        computer.innerHTML = '<img src="assets/paper.png" alt="" class="cpu_display">';
+        result.innerHTML = "It's A Draw!";
+        playerwins++;
+        player_score.innerHTML = 
+     } else if(cpuchoice == 2) {
+        computer.innerHTML = '<img src="assets/rock.png" alt="" class="cpu_display">'
+        result.innerHTML = "Paper Covers Rock, You Win!"
+        
+     } else if(cpuchoice == 3) {
+        computer.innerHTML = '<img src="assets/scissors.png" alt="" class="cpu_display">'
+     } else if(cpuchoice == 4) {
+        computer.innerHTML = '<img src="assets/microwave.png" alt="" class="cpu_display_micro">'
+     } else {
+        computer.innerHTML = '<img src="assets/tin_foil.png" alt="" class="cpu_display">'
+     }
 }
 function pickrock() {
     player.innerHTML = '<img src="assets/rock.png" alt="" class="display">'
