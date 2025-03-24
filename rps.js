f9b0fe531c240c493b825f15739c96893a714437
esca0063@student.acitech.org <esca0063@student.acitech.org>
2025-03-24 09:12:34 -0400

    add animation

--- rps.js
+++ rps.js
@@ -10,18 +10,26 @@ let player_score = document.getElementById("player_score")
 let playerwins = 0
 let computer = document.getElementById("computer")
 let result = document.getElementById("result")
-
+let cpu_score = document.getElementById("computer_score")
+let cpuwins = 0
 for (let index = 0; index < buttons.length; index++) {
     buttons[index].addEventListener("click", () => {
         buttons.forEach(Element => {
             Element.style.display = "none"
+            setTimeout(displayagain, 4000)
+            
         })
     })
 }
-
+function displayagain(){
+        buttons.forEach(Element => {
+            Element.style.display = "flex"
+        })
+}
 
 function computerchoice() {
     return Math.floor(Math.random() * (5-1)) + 1
+      
 }
 
 
@@ -34,18 +42,28 @@ function pickpaper() {
      if (cpuchoice == 1 ) {
         computer.innerHTML = '<img src="assets/paper.png" alt="" class="cpu_display">';
         result.innerHTML = "It's A Draw!";
-        playerwins++;
-        player_score.innerHTML = 
+        
      } else if(cpuchoice == 2) {
         computer.innerHTML = '<img src="assets/rock.png" alt="" class="cpu_display">'
         result.innerHTML = "Paper Covers Rock, You Win!"
-        
+        playerwins++;
+        player_score.innerHTML = "Player score: " + playerwins
      } else if(cpuchoice == 3) {
         computer.innerHTML = '<img src="assets/scissors.png" alt="" class="cpu_display">'
+        result.innerHTML = "Scissors cuts Paper, You Lose!"
+        cpuwins++
+        cpu_score.innerHTML = "Computer score: " + cpuwins
+
      } else if(cpuchoice == 4) {
         computer.innerHTML = '<img src="assets/microwave.png" alt="" class="cpu_display_micro">'
+        result.innerHTML = "Microwave Burns Paper, You Lose!"
+        cpuwins++
+        cpu_score.innerHTML = "Computer score: " + cpuwins
      } else {
         computer.innerHTML = '<img src="assets/tin_foil.png" alt="" class="cpu_display">'
+        result.innerHTML = "Paper Is more Durable than tin foil, You Win!"
+        playerwins++;
+        player_score.innerHTML = "Player score: " + playerwins
      }
 }
 function pickrock() {
